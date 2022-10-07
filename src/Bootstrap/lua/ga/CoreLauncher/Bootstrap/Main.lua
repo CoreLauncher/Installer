5906f2f8a2ffa789b701db146fd81f3fb0aa05ba
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-07 17:01:18 +0200

    Update Main.lua

--- src/Bootstrap/lua/ga/CoreLauncher/Bootstrap/Main.lua
+++ src/Bootstrap/lua/ga/CoreLauncher/Bootstrap/Main.lua
@@ -1,7 +1,25 @@
 print("Hello World!")
 
 _G.CoreLauncher = {}
-CoreLauncher.Http = Import("ga.CoreLauncher.Installer.Libraries.Http")
+CoreLauncher.Http = Import("ga.CoreLauncher.Bootstrap.Libraries.Http")
 
-TypeWriter.Logger.Info("Starting installation process")
-Import("ga.CoreLauncher.Installer.Install")
+if CoreLauncher.Http.HasAccess() then
+    TypeWriter.Logger.Info("Checking for updates")
+    Import("ga.CoreLauncher.Bootstrap.Check")
+else
+    TypeWriter.Logger.Warn("Skipping update check: No internet")
+end
+
+require("coro-spawn")(
+    TypeWriter.This,
+    {
+        args = {
+            "execute", "--input=" .. TypeWriter.ApplicationData .. "/CoreLauncher/CoreLauncher.twr"
+        },
+        stdio = {
+            process.stdin.handle,
+            process.stdout.handle,
+            process.stderr.handle
+        }
+    }
+).waitExit()
